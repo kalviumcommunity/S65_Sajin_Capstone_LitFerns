33e05c18b92c5d6fa9e874c4ad09fc29039c1169
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-07-15 20:57:46 +0530

    feat: User and Book mongoose schemas

---
+++ server/models/Book.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema(
+    {
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        genre: {
+            type: String,
+            required: true,
+        },
+        condition: {
+            type: String,
+            required: true,
+        },
+        isAvailable: {
+            type: Boolean,
+            required: true,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
