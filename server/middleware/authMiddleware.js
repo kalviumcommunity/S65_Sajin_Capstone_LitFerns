cd35529732110cd4826bfee4614fef30dcfc4bdb
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-08-04 15:58:28 +0530

    fix: Enhance security and logic in user API

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -33,7 +33,7 @@ const admin = (req, res, next) => {
     if (req.user && req.user.isAdmin) {
         next();
     } else {
-        res.status(401);
+        res.status(403);
         throw new Error('Not authorized as an admin');
     }
 };
