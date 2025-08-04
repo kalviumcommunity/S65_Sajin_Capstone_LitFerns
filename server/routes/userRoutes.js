3bcc7993c072702c29439953ae77a8b63b98d389
Sajin Saji George <sajinsajigeorge@gmail.com>
2025-08-04 15:45:32 +0530

    refactor: Improve user API responses and security

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, registerUser } = require('../controllers/userController');
+const {
+    authUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
+} = require('../controllers/userController');
+const { protect, admin } = require('../middleware/authMiddleware');
 
-router.route('/').post(registerUser);
+// Routes for registering and getting all users
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 
+// Route for login
 router.post('/login', authUser);
+
+// Routes for user profile
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+
+// Routes for a specific user (Admin only)
+router.route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 
 module.exports = router;
